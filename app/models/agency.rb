2915d2253416d1380ffa5bf09cb19a10bfa50ac5
Hannes Fostie <hannes@openminds.be>
2013-09-05 19:55:49 +0200

    Creates super_users association for Agency

--- app/models/agency.rb
+++ app/models/agency.rb
@@ -2,4 +2,5 @@ class Agency < ActiveRecord::Base
   attr_accessible :city, :country, :email, :name, :agency_id
   has_many :brands
   has_many :users
+  has_many :super_users, class_name: 'User', conditions: { super_user: true }
 end
